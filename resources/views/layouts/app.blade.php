25ba64ae6ba3778dc61a1fdb4c7a974304c456f9
Gerson Stick <stickgerson@gmail.com>
2018-11-18 13:43:21 -0500

    logo en barra de navegación

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,8 @@
     <link href="{{ asset('css/own.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
+    <link rel="icon" type="image/x-icon" href="http://localhost/macad/public/img/logo.png">
+
 
 </head>
 <!--round-->
